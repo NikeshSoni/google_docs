13d2e88d323412525461368adfa95a8f8b5aae5d
Nikesh Rajbhar <nikesh21soni@gmail.com>
2025-03-03 07:56:07 +0530

    Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
+    <div className='flex min-h-screen items-center justify-center'>
         Nikesh Code is Here 
     </div>
   )
