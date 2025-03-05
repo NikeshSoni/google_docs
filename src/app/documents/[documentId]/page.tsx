06185cc37f621549f9def067e5be2ddfafa391ec
Nikesh Rajbhar <nikesh21soni@gmail.com>
2025-03-05 15:48:47 +0530

    Installation of tiptap and setup the code base

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,3 +1,5 @@
+import Editor from "./editor";
+
 
 interface DocumentIdPageProps {
     params: Promise<{ documentId: String }>
@@ -7,8 +9,10 @@ const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
     const { documentId } = await params;
 
     return (
-        <div>
-            DocumentIdPage: {documentId}
+        <div className="him-h-screen bg-[#FAFBFD]">
+            {/* DocumentIdPage: {documentId} */}
+
+            <Editor />
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+// 'use client'
 import React from 'react'
+import Link from 'next/link'
 
 const Home = () => {
   return (
     <div className='flex min-h-screen items-center justify-center'>
-        Nikesh Code is Here 
+        Click <Link href="/documents/123">Here</Link>Go to Document Id 
     </div>
   )
 }
