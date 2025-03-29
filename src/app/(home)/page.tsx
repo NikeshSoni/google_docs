ee0a9525b4be74eb47f2efbdc8839076be76fb98
Nikesh Rajbhar <nikesh21soni@gmail.com>
2025-03-30 02:49:12 +0530

    Enhance schema definition for documents by adding optional fields and indexes; update Home page to fetch and display document titles using Convex queries.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,24 @@
+"use client"
 import React from 'react'
-import Link from 'next/link'
 import { Navbar } from './navbar';
 import { TamplateGallery } from './template-gallery';
+import { useQuery } from 'convex/react';
+import { api } from '../../../convex/_generated/api';
+import { Id } from '../../../convex/_generated/dataModel';
+
+interface Document {
+  _id: Id<"documents">;
+  _creationTime: number;
+  title: string;
+}
 
 const Home = () => {
+  const documents = useQuery(api.documents.get);
+
+  if (documents === undefined) {
+    return <p>Loading...</p>
+  }
+
   return (
     <div className='min-h-screen flex flex-col '>
       <div className='fixed top-0 left-0 right-0 z-10 h-16 bg-white'>
@@ -11,7 +26,15 @@ const Home = () => {
       </div>
 
       <div className='mt-16'>
-          <TamplateGallery />
+        <TamplateGallery />
+
+        {documents?.map((document: Document) => {
+          return (
+            <div key={document._id}>
+              <span>{document.title}</span>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
