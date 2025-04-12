77e33905ad88abc50843d8527e03766088a9d4e4
Nikesh Rajbhar <nikesh21soni@gmail.com>
2025-04-12 07:41:56 +0530

    Refactor DocumentIdPage component by removing unused imports and commented-out code for improved clarity and maintainability.

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,10 +1,6 @@
 import { Document } from "./documents";
 import { auth } from "@clerk/nextjs/server";
 import { preloadQuery } from "convex/nextjs";
-import Editor from "./editor";
-import Navbar from "./navbar";
-import { Room } from "./room";
-import ToolBar from "./ToolBar";
 import { api } from "../../../../convex/_generated/api";
 import { Id } from "../../../../convex/_generated/dataModel";
 
@@ -29,26 +25,10 @@ const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
         { token }
     )
 
-    // if (!preloadedDocument) {
-    //     throw new Error("Document not found");
-    // }
-
     return (
-
         <>
             <Document preloadedDocument={preloadedDocument} />
         </>
-        // <Room>
-        //     <div className="him-h-screen bg-[#FAFBFD]">
-        //         <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#FAFBFD] print:hidden">
-        //             <Navbar />
-        //             <ToolBar />
-        //         </div>
-        //         <div className="pt-[114px] print:pt-0">
-        //             <Editor />
-        //         </div>
-        //     </div>
-        // </Room>
     )
 }
 
